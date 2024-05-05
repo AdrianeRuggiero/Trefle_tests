d55d8e405d80d8e32e62eae68693d42e925334eb
AdrianeRuggiero <driketes@gmail.com>
2024-05-05 18:04:35 +0200

    👌 IMPROVE: Applied flake8 to file searchByName

--- searchByName.py
+++ searchByName.py
@@ -5,6 +5,7 @@ import os
 # Carregar variáveis de ambiente do arquivo .env
 load_dotenv()
 
+
 def get_plants_by_common_name(common_name, auth_token):
     url = "https://trefle.io/api/v1/plants"
     params = {
@@ -19,15 +20,19 @@ def get_plants_by_common_name(common_name, auth_token):
         print("Error:", response.text)
         return None
 
+
 def print_plant_info(plants):
     for plant in plants:
         print("Scientific Name:", plant.get("scientific_name", "N/A"))
         print("Image URL:", plant.get("image_url", "N/A"))
         print()
 
+
 if __name__ == "__main__":
     common_name = "Lavender"
-    plant_data = get_plants_by_common_name(common_name, os.getenv("AUTH_TOKEN"))
+    plant_data = get_plants_by_common_name(
+        common_name, os.getenv("AUTH_TOKEN")
+    )
     if plant_data:
         print_plant_info(plant_data)
     else:
